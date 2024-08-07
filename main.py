8a7405d4a58a3706febc94d95d86177f84567229
frank <likeflyme@gmail.com>
2024-08-07 14:22:35 +0800

    优化main.py，支持 pyinstaller

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from config.app_config import settings
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host=settings.server_host, port=settings.server_port)
+    uvicorn.run(app=app, host=settings.server_host, port=settings.server_port)
